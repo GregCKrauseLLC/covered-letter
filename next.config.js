dfa5d224130926b20e5d9ba2dcb502a1903ead72
GregCKrause <gregckrause@gmail.com>
2023-07-26 08:30:00 -0400

    chore: remove unused next image config

--- next.config.js
+++ next.config.js
@@ -1,15 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "avatars.githubusercontent.com",
-        port: "",
-        pathname: "/u/**",
-      },
-    ],
-  },
   // https://nextjs.org/docs/app/api-reference/next-config-js/output#automatically-copying-traced-files
   output: "standalone",
 };
